19bcaba4366dc9f8dc3a2826c47a069d15fb6a38
Alexander K <xpyro@ya.ru>
2013-04-18 20:41:41 +0600

    inside object spec

--- spec/exmaple_spec.rb
+++ spec/exmaple_spec.rb
@@ -1,7 +1,7 @@
 describe [DefDsl,DefDSL].join' or ' do
   after { remove_const :My if defined? My }
 
-  example 'in module' do
+  example 'usage inside module' do
     module My
       # nice looking recursive dsl definition
       class File < Struct.new :name; end
@@ -30,5 +30,39 @@ describe [DefDsl,DefDSL].join' or ' do
       inner.send(:so2,:dir).map(&:name).should == ['empty']
       inner.send(:so2,:file).map(&:name).should == ['any']
     end
+  end
+
+  example 'usage inside object' do
+    class My
+
+      class File < Struct.new :name; end
+      class Dir < Struct.new :name
+        Dir = Dir
+        File = File
+      end
+
+      extend DefDSL
+      def_dsl Dir
+      def_dsl File      
+
+      def initialize
+        file 'root.txt'
+        dir 'dir' do
+          dir 'inner' do
+            dir 'empty'
+            file 'any'
+          end
+        end
+
+        so.should == {file: so1(:file), dir: so1(:dir)}
+
+        so1(:file).name.should == 'root.txt'
+        so1(:dir).name.should == 'dir'
+        (inner = so1(:dir).send(:so1,:dir)).name.should == 'inner'
+        inner.send(:so2,:dir).map(&:name).should == ['empty']
+        inner.send(:so2,:file).map(&:name).should == ['any']        
+      end
+    end
+    My.new
   end
 end
